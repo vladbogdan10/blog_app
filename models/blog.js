a5d3ca84b08f26fac2a56fcb5e2404ba3bda9ab2
Vlad Bogdan <vlad.bogdan90@gmail.com>
2018-06-24 20:18:00 +0200

    add user relation to blog schema

--- models/blog.js
+++ models/blog.js
@@ -5,7 +5,14 @@ var blogSchema = new mongoose.Schema(
     title: String,
     image: String,
     body: String,
-    created: {type: Date, default: Date.now()}
+    created: {type: Date, default: Date.now()},
+    author: {
+      id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+      },
+      username: String
+    }
   }
 );
 
