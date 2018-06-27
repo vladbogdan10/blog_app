062ab4018828e459cfd1803603657727acb011fd
Vlad Bogdan <vlad.bogdan90@gmail.com>
2018-06-27 09:14:57 +0200

    move to separate folder

--- routes/user.js
+++
@@ -1,9 +0,0 @@
-var express    = require('express'),
-    router     = express.Router();
-    // middleWare = require('../middleware/middleware');
-
-router.get('/user/:id', function(req, res) {
-  res.render('admin-panel');
-});
-
-module.exports = router;
