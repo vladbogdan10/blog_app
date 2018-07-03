2de7a8927c864f43dfcde9cb5f1b44e1e0523169
Vlad Bogdan <vlad.bogdan90@gmail.com>
2018-07-03 09:04:23 +0200

    client side form validation

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,12 @@
+// ADD CLASS ACTIVE TO NAVABR
+$('.menu .item').on('click', function() {
+  $('.menu .item').removeClass('active');
+  $(this).addClass('active');
+}); 
+
 // Prompt user on delete
 $('#del-btn').click(function() {
+  event.preventDefault();
   $('.ui.modal')
   .modal('show')
   .modal({
@@ -7,5 +14,48 @@ $('#del-btn').click(function() {
       $('#del-form').submit();
     }
   });
-  event.preventDefault();
+});
+
+// Close pop-up messages
+$('.message .close').on('click', function() {
+  $(this)
+  .closest('.message')
+  .transition('fade');
+});
+
+// FORM VALIDATION
+$('.ui.form').form({
+  fields: {
+    username: {
+      identifier: 'username',
+      rules: [{
+        type: 'empty',
+        prompt: 'Please enter your username'
+      }]
+    },
+    email: {
+      identifier: 'email',
+      rules: [{
+          type: 'empty',
+          prompt: 'Please enter your e-mail'
+        },
+        {
+          type: 'email',
+          prompt: 'Please enter a valid e-mail'
+        }
+      ]
+    },
+    password: {
+      identifier: 'password',
+      rules: [{
+          type: 'empty',
+          prompt: 'Please enter your password'
+        },
+        {
+          type: 'length[6]',
+          prompt: 'Your password must be at least 6 characters'
+        }
+      ]
+    }
+  }
 });
