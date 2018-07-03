67e799f1cf6fbaea988f6c722d73ffab5943922c
Vlad Bogdan <vlad.bogdan90@gmail.com>
2018-07-03 09:03:43 +0200

    admin key user schema

--- models/user.js
+++ models/user.js
@@ -1,13 +1,12 @@
 var mongoose = require('mongoose');
 var passportLocalMongoose = require('passport-local-mongoose'); 
 
-var UserSchema = new mongoose.Schema(
-  {
-    username: String,
-    email: String,
-    password: String
-  }
-);
+var UserSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
+  admin: Boolean
+});
 
 UserSchema.plugin(passportLocalMongoose, {usernameField: 'email'});
 
