b57ade4ae3b1c622132decc18e696e573583494d
Vlad Bogdan <vlad.bogdan90@gmail.com>
2018-07-03 09:01:57 +0200

    add comments to blog schema

--- models/blog.js
+++ models/blog.js
@@ -1,19 +1,22 @@
 var mongoose = require('mongoose');
 
-var blogSchema = new mongoose.Schema(
-  {
-    title: String,
-    image: String,
-    body: String,
-    created: {type: Date, default: Date.now()},
-    author: {
-      id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-      },
-      username: String
+var blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    username: String
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Comment'
     }
-  }
-);
+  ]
+}, {timestamps: true});
 
 module.exports = mongoose.model('Blog', blogSchema);
