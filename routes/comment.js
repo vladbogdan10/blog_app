743432ca3d95e06ac8fd4a3c678ea8de8cc55298
Vlad Bogdan <vlad.bogdan90@gmail.com>
2018-07-03 09:05:43 +0200

    comment route

---
+++ routes/comment.js
@@ -0,0 +1,39 @@
+var express      = require('express'),
+    router       = express.Router(),
+    Blog         = require('../models/blog'),
+    Comment      = require('../models/comment'),
+    middleWare   = require('../middleware/middleware'),
+    sanitizeHtml = require('sanitize-html');
+
+
+router.post('/blogs/:id/comments', middleWare.isLoggedIn, function(req, res) {
+  Blog.findById(req.params.id, function(err, blog) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (!req.body.comment) {
+        req.flash('error', 'Your comment could not be submited because was empty!');
+        res.redirect('/blogs/' + blog._id);
+      } else {
+        req.body.comment.text = sanitizeHtml(req.body.comment.text);
+        Comment.create(req.body.comment, function(err, comment) {
+          if (err) {
+            req.flash('error', 'Something went wrong');
+            console.log(err);
+            res.redirect('/blogs/' + blog._id);
+          } else {
+            comment.author.id = req.user._id;
+            comment.author.username = req.user.username;
+            comment.save();
+            blog.comments.push(comment);
+            blog.save();
+            req.flash('success', 'Your comment was successfully added');
+            res.redirect('/blogs/' + blog._id);
+          }
+        });
+      }
+    }
+  });
+});
+
+module.exports = router;
