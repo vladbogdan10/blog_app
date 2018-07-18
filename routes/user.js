101fd419d1d566a0ddd64e3dc6bbbb33dd451439
Vlad Bogdan <vlad.bogdan90@gmail.com>
2018-07-18 10:33:03 +0200

    User update logic

--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,12 @@ var express    = require('express'),
     User       = require('../models/user'),
     middleWare = require('../middleware/middleware');
 
-
+// INDEX ROUTE
 router.get('/user', middleWare.isLoggedIn, function(req, res) {
   res.redirect('/user/' + req.user.id);
 });
 
+// USER ROUTE
 router.get('/user/:id', middleWare.isOwner, function(req, res) {
   User.findById(req.params.id, function(err, user) {
     if (err) {
@@ -18,12 +19,45 @@ router.get('/user/:id', middleWare.isOwner, function(req, res) {
   });
 });
 
+// UPDATE USER ROUTE
 router.put('/user/:id', function(req, res) {
-  res.send('update route');
+  User.findOneAndUpdate({_id: req.user._id}, req.body.user, {upsert: true},
+    function(err, foundUser) {
+      if (err) {
+        req.flash('error', 'Email address already taken!');
+        res.redirect('back');
+      } else {
+        foundUser.changePassword(req.body.user.oldPassword, 
+          req.body.user.newPassword, function(passwordErr) {
+          if (passwordErr) {
+            req.flash('error', passwordErr.message);
+            res.redirect('back');
+          } else {
+            foundUser.save(function(error) {
+              if (error) {
+                console.log(error);
+              } else {
+                req.flash('success', 'Successfully updated!');
+                res.redirect('back');
+              }
+            });
+          }
+        });
+      }
+    });
 });
 
+// DELETE USER ROUTE
 router.delete('/user/:id', function(req, res) {
-  res.send('delete route');
+  User.findByIdAndRemove(req.user._id, function(err) {
+    if (err) {
+      req.flash('error', 'Something went wrong! Try again!');
+      res.redirect('back');
+    } else {
+      req.flash('success', 'Your account has been deleted!');
+      res.redirect('/');
+    }
+  });
 });
 
 module.exports = router;
