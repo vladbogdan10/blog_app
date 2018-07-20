d716ad9fe7418c07baf9e18f97a24617ad414b4d
Vlad Bogdan <vlad.bogdan90@gmail.com>
2018-07-20 13:57:51 +0200

    add route for uploading user image

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,9 @@
 var express    = require('express'),
     router     = express.Router(),
     User       = require('../models/user'),
-    middleWare = require('../middleware/middleware');
+    middleWare = require('../middleware/middleware'),
+    multer     = require('multer'),
+    upload     = multer({dest: 'public/uploads/profile-images'});
 
 // INDEX ROUTE
 router.get('/user', middleWare.isLoggedIn, function(req, res) {
@@ -56,6 +58,24 @@ router.delete('/user/:id', function(req, res) {
     } else {
       req.flash('success', 'Your account has been deleted!');
       res.redirect('/');
+    }
+  });
+});
+
+router.post('/user/avatar', upload.single('img_upload'), function(req, res) {
+  User.findById(req.user._id, function(err, user) {
+    if (err) {
+      req.flash('error', 'Something went wrong! Try again!');
+      res.redirect('back');
+    } else {
+      if (!req.file) {
+        req.flash('error', 'Please choose an image!');
+        res.redirect('back');
+      } else {
+        user.img = req.file.path;
+        user.save();
+        res.redirect('back');
+      }
     }
   });
 });
